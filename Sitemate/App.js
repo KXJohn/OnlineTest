637f35c583bd0f781e98a0d9046a2063a29de68e
KaixiangJony <kaixiangjohn@gmail.com>
2024-07-20 11:03:59 +0930

    Display result in flatList

--- Sitemate/App.js
+++ Sitemate/App.js
@@ -1,9 +1,11 @@
-import {useCallback, useEffect, useState} from 'react';
-import { FlatList, TextInput, View, StyleSheet, Button } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { FlatList, TextInput, View, StyleSheet, Button, Text } from 'react-native';
 
 
 const API_KEY = '479dc08a1d384091b59557f2d9c3dd23';
 const BASE_URL = 'https://newsapi.org/v2/everything?';
+
+const keyExtractor = (item) => item.url;
 
 export default function App() {
   const [searchText, setSearchText] = useState('tesla');
@@ -23,22 +25,31 @@ export default function App() {
     }
   };
 
-
-  console.log(result);
+  const renderItem = ({ item, index }) => {
+    return (
+        <View style={styles.itemContainer}>
+          <Text numberOfLines={2} ellipsizeMode='tail' style={styles.title}>{index} Title: {item.title}</Text>
+          <Text style={styles.title}> Author: {item.author} </Text>
+          <Text numberOfLines={1} ellipsizeMode='tail'> Content : {item.content}</Text>
+        </View>
+        );
+  };
   
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
-      <TextInput
-        style={styles.input}
-        onChangeText={setSearchText}
-        value={searchText}
-      />
-      <Button
-        title="Search"
-        onPress={() => onPress()}
-      />
+        <TextInput
+          style={styles.input}
+          onChangeText={setSearchText}
+          value={searchText}
+        />
+        <Button
+          title="Search"
+          onPress={() => onPress()}
+        />
       </View>
+      <Text style={styles.title}> Count: {result.length} </Text>
+      <FlatList data={result} renderItem={renderItem} keyExtractor={keyExtractor} />
     </View>
   );
 }
@@ -46,6 +57,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -54,20 +66,29 @@ const styles = StyleSheet.create({
   searchContainer: {
     flexDirection: 'row',
     alignContent: 'center',
-    height: 40,
+    marginBottom: 20,
   },
 
   input: {
     height: 40,
-    margin: 12,
     borderWidth: 1,
-    padding: 10,
+    marginRight: 20,
   },
+
   text: {
     fontSize: 16,
   },
-  wrapperCustom: {
-    borderRadius: 8,
-    padding: 6,
+
+  itemContainer: {
+    width: 550,
+    height: 100,
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 5,
+    alignContent: 'center'
   },
+
+  title : {
+    fontWeight: 600
+  }
 });
