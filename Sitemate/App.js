4c5b2a9f1321b8393beded7095e4a1a5d7b7e5a0
KaixiangJony <kaixiangjohn@gmail.com>
2024-07-20 10:34:33 +0930

    build the UI

--- Sitemate/App.js
+++ Sitemate/App.js
@@ -1,16 +1,18 @@
-import {useEffect, useState} from 'react';
-import {FlatList, Text, View, StyleSheet} from 'react-native';
+import {useCallback, useEffect, useState} from 'react';
+import { FlatList, TextInput, View, StyleSheet, Button } from 'react-native';
 
+
+const API_KEY = '479dc08a1d384091b59557f2d9c3dd23';
+const BASE_URL = 'https://newsapi.org/v2/everything?';
 
 export default function App() {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState('tesla');
   const [result, setResult] = useState([]);
-  var url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-06-20&sortBy=publishedAt&apiKey=479dc08a1d384091b59557f2d9c3dd23';
 
-
-  const getResults = async () => {
+  const onPress = async () => {
+    const searchUrl = `${BASE_URL}q=${searchText}&from=2024-06-20&sortBy=publishedAt&apiKey=${API_KEY}`;
     try {
-      const response = await fetch(url);
+      const response = await fetch(searchUrl);
       const json = await response.json();
       setResult(json.articles);
       console.log("json", json);
@@ -22,16 +24,21 @@ export default function App() {
   };
 
 
-  useEffect(() => {
-    getResults();
-  }, []);
-
   console.log(result);
   
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.searchContainer}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setSearchText}
+        value={searchText}
+      />
+      <Button
+        title="Search"
+        onPress={() => onPress()}
+      />
+      </View>
     </View>
   );
 }
@@ -42,5 +49,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  searchContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    height: 40,
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  text: {
+    fontSize: 16,
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6,
   },
 });
